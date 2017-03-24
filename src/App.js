b988bf8e37d1bdd236ded59f01f524c08e4b3259
shapiro_benjamin@bah.com <benjaminshapiro@C1MS50ZQH3QK.local>
2017-03-24 16:30:39 -0400

    complete stopwatch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Stopwatch from "./Stopwatch";
-import './App.css';
 
 class App extends Component {
   render() {
--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
-import "./Stopwatch.css"
+import "./Stopwatch.css";
 
 class Stopwatch extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      seconds: 0,
+      paused: true,
+    }
+    this.timerId = null
+  }
+
+  startButton(e){
+    if(!this.timerId){
+      this.timerId = setInterval( _ => this.countSeconds(), 1000);
+    }
+  }
+
+  pauseButton(e){
+    let paused = !this.state.paused;
+    if(paused){
+      this.resetTimer();
+      this.setState({
+        paused,
+      })
+    }
+    else{
+      this.startButton();
+    }
+  }
+
+  resetButton(e){
+    this.resetTimer();
+    this.setState({
+      seconds: 0,
+      paused: true,
+    })
+  }
+
+  countSeconds(){
+    this.setState({
+      seconds: this.state.seconds +1,
+      paused: false
+    })
+  }
+
+  resetTimer(){
+    clearInterval(this.timerId);
+    this.timerId = null;
+  }
+
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.seconds}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={ e => this.resetButton(e) }>Reset</button>
+          <button onClick={ e => this.startButton(e) }>Start</button>
+          <button onClick={ e => this.pauseButton(e) }>Pause</button>
         </div>
       </div>
     );
